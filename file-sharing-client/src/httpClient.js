32ee247ab562aa33db392ca2a49bcf4737b20eb9
Ashhad Memon <a23memon@uwaterloo.ca>
2026-03-06 22:44:00 -0500

    Add config template, env-based API URL, fix CI coverage and client entry

--- file-sharing-client/src/httpClient.js
+++ file-sharing-client/src/httpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-  baseURL: 'http://localhost:5166',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5166',
 });
 
 export default httpClient;
